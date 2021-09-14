b40340d82f074e8661bf44fc1a311c0a1d348190
FifthRooter <toms@arbestor.xyz>
2021-09-14 17:24:05 +0200

    restructured project

--- background.js
+++ background.js
@@ -9,12 +9,12 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete' && /^http/.test(tab.url)){
         chrome.scripting.insertCSS({
             target: {tabId},
-            files: ['./foreground-styles.css']
+            files: ['./styles/foreground-styles.css']
         })
         .then(() => {
             chrome.scripting.executeScript({
                 target: {tabId},
-                files: ['./foreground.js']
+                files: ['./popboy.js']
             })
             .then(() => {
                  console.log('injected foreground script');

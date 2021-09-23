6663080099025c0b7c0159ec7995667637716407
FifthRooter <toms@arbestor.xyz>
2021-09-23 14:55:40 +0200

    updated webpack config file to copy all other static files from src dir to build dir

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/js/popboy.js",
   output: {
     path: path.resolve(__dirname, "./src/js/"),
-    filename: "popboy.bundle.js",
+    filename: "../../build/js/popboy.bundle.js",
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "./src/",
+          to: "../../build/",
+          globOptions: {
+            ignore: ["**/helpers/**", "**/popboy.*"],
+          },
+        },
+      ],
+    }),
+  ],
 };
